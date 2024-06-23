d4b5483977df4a5024675ed051d1acde06426bef
phanthm <prathamtalware10@gmail.com>
2024-06-24 00:01:16 +0530

    deployment ready

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,5 +1,4 @@
 import useGetConversations from "../../hooks/useGetConversations";
-import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
@@ -10,7 +9,6 @@ const Conversations = () => {
 				<Conversation
 					key={conversation._id}
 					conversation={conversation}
-					emoji={getRandomEmoji()}
 					lastIdx={idx === conversations.length - 1}
 				/>
 			))}
@@ -20,20 +18,3 @@ const Conversations = () => {
 	);
 };
 export default Conversations;
-
-// STARTER CODE SNIPPET
-// import Conversation from "./Conversation";
-
-// const Conversations = () => {
-// 	return (
-// 		<div className='py-2 flex flex-col overflow-auto'>
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 			<Conversation />
-// 		</div>
-// 	);
-// };
-// export default Conversations;
