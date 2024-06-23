1c00deaefe37750a42004ba4510b2534d31eb5a2
phanthm <prathamtalware10@gmail.com>
2024-06-23 16:37:34 +0530

    created the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <Routes>
-        {/* <Route path='/' element={<Home />} /> */}
+        <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
       </Routes>
