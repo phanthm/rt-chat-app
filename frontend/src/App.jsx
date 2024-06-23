403e73f0f302cb24fc517913cb1dde55f181880d
phanthm <prathamtalware10@gmail.com>
2024-06-23 16:18:40 +0530

    created login and signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+// import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { Toaster } from "react-hot-toast";
 import './App.css'
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Routes>
+        {/* <Route path='/' element={<Home />} /> */}
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+      <Toaster />
+    </div>
   )
 }
 
