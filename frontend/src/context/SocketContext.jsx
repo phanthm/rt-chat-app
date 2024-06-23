740f6960803c0d7ab6662b18dd86ae5ceacf63c4
phanthm <prathamtalware10@gmail.com>
2024-06-24 00:20:53 +0530

    deploy url changes for socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://rt-chat-app-jv9w.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
